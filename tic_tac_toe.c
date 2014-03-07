aed425c67822cd9a1f58f5186153de7d39525733
Joe Miller <josephclaymiller@gmail.com>
2014-03-06 20:04:11 -0800

    first commit

---
+++ tic_tac_toe.c
@@ -0,0 +1,192 @@
+//
+//  main.c
+//  Tic Tac Toe
+//
+//  Created by Joseph Miller on 3/2/14.
+//  Copyright (c) 2014 Joseph Miller. All rights reserved.
+//
+
+#include <stdio.h>
+#define ROWS 3
+#define COLS 3
+#define BLANK '_'
+
+void start();
+void takeTurn(char mark, char board[ROWS][COLS]);
+void displayBoard(char board[ROWS][COLS]);
+char *getCoordinates();
+int placeMark(char mark, int x, int y, char board[ROWS][COLS]);
+char winner(char board[ROWS][COLS]);
+char diagonalWinner(char board[ROWS][COLS]);
+char rowWinner(char board[ROWS][COLS]);
+char colWinner(char board[ROWS][COLS]);
+
+int main(int argc, const char * argv[])
+{
+    // Tic Tac Toe
+    printf("Welcome to Tic Tac Toe!\n");
+    start();
+    
+    return 0;
+}
+
+void start()
+{
+    char board[ROWS][COLS] = { BLANK, BLANK, BLANK, BLANK, BLANK, BLANK, BLANK, BLANK, BLANK };
+    char player1 = 'x';
+    char player2 = 'o';
+    char players[2] = { player1, player2 };
+    int currentPlayer = 0;
+    int moves = 0;
+    int numPlayers = sizeof(players);
+    int maxMoves = (ROWS * COLS);
+    char winnerMark;
+    
+    // Show blank board
+    displayBoard(board);
+    
+    // Game loop
+    do {
+        // Take turn
+        takeTurn(players[currentPlayer], board);
+        moves++;
+        
+        // Check if winner
+        winnerMark = winner(board);
+        if (winnerMark != BLANK) {
+            printf("%c won!\n", winnerMark);
+            break;
+        }
+        
+        // Switch players
+        currentPlayer++;
+        currentPlayer %= numPlayers;
+    } while (moves < maxMoves);
+}
+
+void takeTurn(char mark, char board[ROWS][COLS])
+{
+    char *coords;
+    int x, y;
+    int validSpace;
+    coords = getCoordinates();
+    x = coords[0];
+    y = coords[1];
+    validSpace = placeMark(mark, x, y, board);
+    if (validSpace == 0) {
+        displayBoard(board);
+    } else {
+        printf("Invalid move, try again\n");
+        takeTurn(mark, board);
+    }
+}
+
+void displayBoard(char board[ROWS][COLS])
+{
+    int i, j;
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("%c", board[i][j]);
+            if (j < 2) { printf("|"); }
+        }
+        printf("\n");
+    }
+}
+
+char *getCoordinates()
+{
+    int x, y;
+    char coords[2];
+    char *coordPointer;
+    printf( "Please input coordinates: " );
+    scanf( "%d", &x );
+    scanf( "%d", &y );
+    coords[0] = x;
+    coords[1] = y;
+    coordPointer = coords;
+    return coordPointer;
+}
+
+int placeMark(char mark, int x, int y, char board[ROWS][COLS])
+{
+    char boardSpace = board[x][y];
+    if (boardSpace == BLANK) {
+        board[x][y] = mark;
+    } else {
+        return 1;
+    }
+    return 0;
+}
+
+char winner(char board[ROWS][COLS]) {
+    char diagonalWin;
+    char rowWin;
+    char colWin;
+    
+    // Diagonals
+    diagonalWin = diagonalWinner(board);
+    if (diagonalWin != BLANK) {
+        return diagonalWin;
+    }
+    // Rows
+    rowWin = rowWinner(board);
+    if (rowWin != BLANK) {
+        return rowWin;
+    }
+    // Cols
+    colWin = colWinner(board);
+    if (colWin != BLANK) {
+        return colWin;
+    }
+    return BLANK;
+}
+
+char diagonalWinner(char board[ROWS][COLS]) {
+    char diagonal1;
+    char diagonal2;
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] ) {
+        diagonal1 = board[1][1];
+        return diagonal1;
+    }
+    if (board[2][0] == board[1][1] && board[1][1] == board[0][2] ) {
+        diagonal2 = board[1][1];
+        return diagonal2;
+    }
+    return BLANK;
+}
+
+char rowWinner(char board[ROWS][COLS]) {
+    char rowMark;
+    int row, col;
+    for (row = 0; row < ROWS; row++) {
+        rowMark = board[row][0];
+        for (col = 0; col < COLS; col++) {
+            if (board[row][col] != rowMark) {
+                rowMark = BLANK;
+                break;
+            }
+        }
+        if (rowMark != BLANK) {
+            return rowMark;
+        }
+    }
+    return BLANK;
+}
+
+char colWinner(char board[ROWS][COLS]) {
+    char colMark;
+    int col, row;
+    for (col = 0; col < COLS; col++) {
+        colMark = board[0][col];
+        for (row = 0; row < ROWS; row++) {
+            if (board[row][col] != colMark) {
+                colMark = BLANK;
+                break;
+            }
+        }
+        if (colMark != BLANK) {
+            return colMark;
+        }
+    }
+    return BLANK;
+}
